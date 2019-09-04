a1c06efc5908eb8de89ce1a40887465f67d65069
Yasmany Santalla Pereda <ysantalla88@gmail.com>
2019-09-04 14:48:11 -0400

    First version

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -1,8 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID } from '@angular/core';
+
+import { registerLocaleData } from '@angular/common';
+import localeCl from '@angular/common/locales/es-CU';
+
+registerLocaleData(localeCl, 'es-CU');
+
+import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+
+import { CoreModule } from '@app/core/core.module';
+import { SharedModule } from '@app/shared/shared.module';
+import { AuthGuard } from '@app/core/guards/auth.guard';
+
+
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import('./views/+home/home.module').then(m => m.HomeModule)
+  },
+  { path: '**', redirectTo: 'error/unauthorized' }
+];
+
+
 
 @NgModule({
   declarations: [
@@ -10,9 +33,15 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'serverApp' }),
-    BrowserAnimationsModule
+    CoreModule,
+    SharedModule.forRoot(),
+    BrowserAnimationsModule,
+    RouterModule.forRoot(routes,
+      {
+        preloadingStrategy: PreloadAllModules
+      })
   ],
-  providers: [],
+  providers: [ { provide: LOCALE_ID, useValue: 'es-CU' } ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ app/src/app/core/core.module.ts
@@ -0,0 +1,40 @@
+import {
+  NgModule,
+  SkipSelf,
+  Optional,
+} from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import {
+  HttpClientModule,
+} from '@angular/common/http';
+
+import { AuthGuard } from '@app/core/guards/auth.guard';
+import { AuthService } from './services/auth.service';
+import { RedirectGuard } from './guards/redirect.guard';
+
+
+@NgModule({
+  imports: [
+    CommonModule,
+    HttpClientModule,
+  ],
+  providers: [
+    AuthGuard,
+    RedirectGuard,
+    AuthService
+  ],
+  declarations: []
+})
+export class CoreModule {
+
+  constructor(
+    @Optional()
+    @SkipSelf()
+    parentModule: CoreModule,
+  ) {
+    if (parentModule) {
+      throw new Error('CoreModule is already loaded. Import only in AppModule');
+    }
+  }
+}
